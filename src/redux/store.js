a250de3e16c67ea0d90b9b69b86a22e189dadc65
Sean <>
2022-01-29 14:56:42 -0800

    Part 5 - Redux Debugging, New and better way to create store with debuggers fully integrated

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,24 @@
 import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import {
+  composeWithDevTools,
+  devToolsEnhancer,
+} from "redux-devtools-extension";
 import logger from "redux-logger";
 import reducer from "./reducer";
 
-const store = createStore(
-  reducer,
-  composeWithDevTools(applyMiddleware(logger))
-);
+//! New and better way to create store with debuggers fully integrated
+const composeEnhancers = composeWithDevTools({
+  trace: true,
+});
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(logger)));
 
 export default store;
+
+//!  old:
+// const store = createStore(
+//   reducer,
+//   devToolsEnhancer({ trace: true })
+// );
+
+// export default store;
